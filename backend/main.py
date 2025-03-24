ef44054b0f7b880114d21a0747630a392557128f
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2025-03-24 15:27:29 -0400

    - Created first path operation function - Created models for data validation and parsing

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from dotenv import load_dotenv
+from models import HoFGame
+# from example_data import data as example_data
 
-app = FastAPI()
+import os
+import httpx
+import logging
 
+load_dotenv()
+
+# Resources
+app = FastAPI(debug=True)
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+RAPIDAPI_HOST = os.getenv("RAPIDAPI_HOST")
+url = f"https://{RAPIDAPI_HOST}"
+
+headers = {
+	"x-rapidapi-key": f"{RAPIDAPI_KEY}",
+	"x-rapidapi-host": f"{RAPIDAPI_HOST}"
+    }
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Worlds"}
+ 
+@app.get("/hall-of-fame", response_model=list[HoFGame])
+async def get_hall_of_fame() -> HoFGame:
+    try:
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.get(url+ "/game/hall-of-fame", headers=headers)
+                response.raise_for_status()
+                data = response.json()
+                return data
+                         
+            except httpx.HTTPStatusError as e:
+                raise HTTPException(status_code=e.response.status_code, detail="Error fetching data from RapidAPI")
+            
+    except Exception as e:
+        raise HTTPException(status_code=500, detail='yikes')
+
+
---
+++ backend/models.py
@@ -0,0 +1,69 @@
+"""
+- These models are made to validate JSON responses 
+- Game is the root object
+- If a model contains a field that is also a class a model must be made to support it
+"""
+
+from typing import List, Optional
+from datetime import datetime
+from pydantic import BaseModel, Field
+from enum import Enum
+
+
+aliases = {
+    "top_critic_score": 'topCriticScore',
+    "platforms": 'Platforms',
+    "first_release_date": 'firstReleaseDate',
+    "short_name": "shortName",
+    "image_src": "imageSrc",
+    "platform_id":"_id",
+    "release_date": "releaseDate",
+}
+
+
+class Tier(str, Enum):
+   MIGHTY = "Mighty"
+   STRONG = "Strong"
+
+class Banner(BaseModel):
+    og: str
+    sm: str
+
+class Box(BaseModel):
+    og: str
+    sm: str
+   
+class Images(BaseModel):
+    banner: Banner
+    box: Optional[Banner] = None
+
+class Platform(BaseModel):
+    display_release: Optional[datetime] = None
+    id: int
+    image_src: Optional[str] = None
+    name: str
+    release_date: datetime = Field(alias=aliases["release_date"])
+    short_name: str = Field(alias = aliases["short_name"])
+
+
+class Game(BaseModel): # Base model for the ROOT object
+    first_release_date: datetime = Field(alias = aliases["first_release_date"])
+    id: int
+    images: Images
+    name: str
+    platforms: List[Platform] = Field(alias = aliases["platforms"])
+    tags: List[str] 
+    tier: Tier
+    top_critic_score: int = Field(alias = aliases["top_critic_score"])
+
+class HoFGame(BaseModel): # Model for the /hall-of-fame endpoint
+    name: str
+    id: int
+    tier: Tier
+    images: Images # box art
+    platforms: List[Platform] = Field(alias  = aliases["platforms"])
+    top_critic_score: int = Field(alias = aliases["top_critic_score"]) #top score
+
+class Popular(BaseModel):
+    name: str
+    top_critic_score: int = Field(alias = aliases["top_critic_score"])
